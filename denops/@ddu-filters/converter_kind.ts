3cb90398f8b417bfd25df79eefbb7943a222e09f
flow6852 <flow6852math@gmail.com>
2023-07-10 13:09:34 +0000

    formatter

--- denops/@ddu-filters/converter_kind.ts
+++ denops/@ddu-filters/converter_kind.ts
@@ -1,23 +1,23 @@
 import {
   BaseFilter,
   DduItem,
-  SourceOptions,
   KindOptions,
+  SourceOptions,
 } from "https://deno.land/x/ddu_vim@v3.3.3/types.ts";
 import { Denops } from "https://deno.land/x/ddu_vim@v3.3.3/deps.ts";
 
 type KindKeyReplace = {
-    oldKey: string;
-    newKey: string;
+  oldKey: string;
+  newKey: string;
 };
 
 type Params = {
-    kind?: string,
-    kindKeyReplaces?: Array<KindKeyReplace> // 
-    options?: KindOptions, // can change?
-    otionsOverride?: boolean,
-    params?: unknown, // can change?
-    paramsOverride?: boolean, // ?
+  kind?: string;
+  kindKeyReplaces?: Array<KindKeyReplace>; //
+  options?: KindOptions; // can change?
+  otionsOverride?: boolean;
+  params?: unknown; // can change?
+  paramsOverride?: boolean; // ?
 };
 
 export class Filter extends BaseFilter<Params> {
@@ -25,29 +25,35 @@ export class Filter extends BaseFilter<Params> {
   override async filter(args: {
     denops: Denops;
     sourceOptions: SourceOptions;
-    filterParams: Params,
+    filterParams: Params;
     input: string;
     items: DduItem[];
   }): Promise<DduItem[]> {
     for (const item of args.items) {
-        if (args.filterParams.kind != undefined) item.kind = args.filterParams.kind;
-        // if (args.filterParams.kindKeyReplaces) { 
-        //     for (const replacer of args.filterParams.kindKeyReplaces as Array<KindKeyReplace>){
-        //         item.action[replacer.newKey] = item.action[replacer.oldKey] // "." is ignored for using dict member? then format : ([0-9a-zA-Z]*.)*
-        //     }
-        // }
+      if (args.filterParams.kind != undefined) {
+        item.kind = args.filterParams.kind;
+      }
+      // if (args.filterParams.kindKeyReplaces) {
+      //     for (const replacer of args.filterParams.kindKeyReplaces as Array<KindKeyReplace>){
+      //         item.action[replacer.newKey] = item.action[replacer.oldKey] // "." is ignored for using dict member? then format : ([0-9a-zA-Z]*.)*
+      //     }
+      // }
     }
     return Promise.resolve(args.items);
   }
 
   override params(): Params {
     return {
-        kind: undefined,
-        kindKeyReplaces: undefined,
-        options: undefined,
-        otionsOverride: undefined,
-        params: undefined,
-        paramsOverride: undefined
+      kind: undefined,
+      kindKeyReplaces: undefined,
+      options: undefined,
+      otionsOverride: undefined,
+      params: undefined,
+      paramsOverride: undefined,
     };
   }
+}
+
+function parseKeys(keys: string): Array<string> {
+    return keys.split
 }
