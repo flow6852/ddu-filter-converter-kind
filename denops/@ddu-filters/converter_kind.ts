7f9a4465f0dd5f627866752a8b388dea05a9e7b4
flow6852 <flow6852math@gmail.com>
2024-01-30 22:04:41 +0900

    Update

--- denops/@ddu-filters/converter_kind.ts
+++ denops/@ddu-filters/converter_kind.ts
@@ -2,8 +2,8 @@ import {
   BaseFilter,
   DduItem,
   SourceOptions,
-} from "https://deno.land/x/ddu_vim@v3.6.0/types.ts";
-import { Denops } from "https://deno.land/x/ddu_vim@v3.6.0/deps.ts";
+} from "https://deno.land/x/ddu_vim@v3.9.0/types.ts";
+import { Denops } from "https://deno.land/x/ddu_vim@v3.9.0/deps.ts";
 
 type ActionData = {
   [K in string]: string;
